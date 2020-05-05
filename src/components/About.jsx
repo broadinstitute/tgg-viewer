02fc470aa5e56014dd1593b142689b6e68f1ef5c
bw2 <weisburd@broadinstitute.org>
2020-05-05 01:46:02 -0400

    updated About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,13 +23,13 @@ const AboutLink = () => (
     <br />
     Main features: <br />
     <ul>
-      <li>Supports all file formats supported by IGV.js, including: .bed, .vcf, .bam, .cram, .bigWig, as well as
+      <li>Supports most file formats supported by IGV.js, including: .bed, .vcf, .bam, .cram, .bigWig, as well as
         several custom formats for splice junctions (.junctions.bed.gz) and gCNV copy-number variants (.gcnv.bed) </li>
       <li>Includes reference tracks such as splice junctions from GTEx v8 muscle, blood, and fibroblast samples.</li>
-      <li>Loads data files or reference tracks from any Google bucket to which you have read-access.</li>
-      <li>Retrieves just the on-screen sections of the files directly from Google buckets.</li>
-      <li>Allows gene or locus lists to be added to side bars for quick navigation across a list of regions.</li>
-      <li>Fully customizable via .json config file - for example: TODO.</li>
+      <li>Can load data files or reference tracks from any Google bucket to which you have read-access.</li>
+      <li>Reads just the on-screen sections of the files directly from Google buckets.</li>
+      <li>Supports adding gene or locus lists to left or right side bar for quick navigation across a list of regions.</li>
+      <li>All settings and data paths can be customized and shared via .json config files.</li>
     </ul>
 
     <a href="https://github.com/broadinstitute/tgg-viewer/issues" target="_blank">Issues</a>
