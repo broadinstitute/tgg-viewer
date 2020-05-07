d18fb346371d23ec9ce9bd484e81e61b2d53964d
bw2 <weisburd@broadinstitute.org>
2020-05-06 22:30:43 -0400

    refactored Search

--- src/components/SearchRows.jsx
+++ src/components/SearchRows.jsx
@@ -9,7 +9,8 @@ import styled from 'styled-components'
 import { Search } from 'semantic-ui-react'
 import {
   getRowsInCategories,
-  getSelectedRowNamesByCategoryName, getSelectedSamplesByCategoryNameAndRowName,
+  getSelectedRowNamesByCategoryName,
+  getSelectedSamplesByCategoryNameAndRowName,
 } from '../redux/selectors'
 
 const StyledSearch = styled(Search)`
@@ -42,7 +43,7 @@ const StyledSearch = styled(Search)`
   }
 `
 
-const MAX_AUTOCOMPLETE_RESULTS = 10
+const MAX_AUTOCOMPLETE_RESULTS = 12
 const INITIAL_STATE = { isLoading: false, results: [], value: '' }
 
 class SearchRows extends React.Component {
@@ -54,8 +55,6 @@ class SearchRows extends React.Component {
 
   handleResultSelect = (e, { result }) => {
     this.setState({ value: '' })
-
-    console.log(e, result)
 
     const idTokens = result.id.split('!!')
     const action = idTokens[0]
@@ -73,15 +72,129 @@ class SearchRows extends React.Component {
     }
   }
 
+  getSelectedRowNames = (categoryName) => (this.props.selectedRowNamesByCategoryName[categoryName] || [])
+
+  isRowSelected = (categoryName, rowName) => this.getSelectedRowNames(categoryName).includes(rowName)
+
+  getSelectedSamplesByRowName = (categoryName) => ((this.props.selectedSamplesByCategoryNameAndRowName[categoryName] || {}).selectedSamples || {})
+
+  getSelectedSamplesForRow = (categoryName, rowName) => (this.getSelectedSamplesByRowName(categoryName)[rowName] || [])
+
+  isSampleSelected = (categoryName, rowName, sample) => this.getSelectedSamplesForRow(categoryName, rowName).includes(sample)
+
+  addRowsToHide = (resultsByCategoryName, resultsCounter) => {
+    Object.keys(this.props.selectedRowNamesByCategoryName).forEach((categoryName) => {
+      if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
+      this.props.selectedRowNamesByCategoryName[categoryName].forEach((rowName) => {
+        if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
+        resultsByCategoryName[categoryName] = resultsByCategoryName[categoryName] || []
+        const newResult = { action: 'REMOVE', categoryName, rowName }
+        if (!_.some(resultsByCategoryName[categoryName], newResult)) {
+          resultsByCategoryName[categoryName].push(newResult)
+          resultsCounter += 1
+        }
+      })
+    })
+
+    return resultsCounter
+  }
+
+  addSamplesToHide = (resultsByCategoryName, resultsCounter) => {
+    Object.keys(this.props.selectedSamplesByCategoryNameAndRowName).forEach((categoryName) => {
+      if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
+      Object.keys(this.getSelectedSamplesByRowName(categoryName)).forEach((rowName) => {
+        if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS || !this.isRowSelected(categoryName, rowName)) return
+        this.getSelectedSamplesForRow(categoryName, rowName).forEach((sample) => {
+          if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
+          resultsByCategoryName[categoryName] = resultsByCategoryName[categoryName] || []
+          const newResult = { action: 'REMOVE', categoryName, rowName, sample }
+          if (!_.some(resultsByCategoryName[categoryName], newResult)) {
+            resultsByCategoryName[categoryName].push(newResult)
+            resultsCounter += 1
+          }
+        })
+      })
+    })
+
+    return resultsCounter
+  }
+
+  addMatchingSamplesFromRow = (categoryName, row, inputStringRegExp, resultsByCategoryName, resultsCounter) => {
+    if (!row.data) {
+      return resultsCounter
+    }
+
+    const dataItemsWithSamples = row.data.filter((data) => data.samples && data.samples.length > 0)
+    if (dataItemsWithSamples.length > 1) {
+      console.warn('Found row with multiple data items each of which has a samples array. This may behave unexpectedly if there are duplicate samples')
+    }
+
+    //add samples with matching names
+    dataItemsWithSamples.forEach((data) => {
+      (data.samples || []).forEach((sample) => {
+        if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
+        if (inputStringRegExp.test(sample)) {
+          resultsByCategoryName[categoryName] = resultsByCategoryName[categoryName] || []
+          const newResult = {
+            action: this.isRowSelected(categoryName, row.name) && this.isSampleSelected(categoryName, row.name, sample) ? 'REMOVE' : 'ADD',
+            categoryName,
+            rowName: row.name,
+            sample,
+          }
+          if (!_.some(resultsByCategoryName[categoryName], newResult)) {
+            resultsByCategoryName[categoryName].push(newResult)
+            resultsCounter += 1
+          }
+        }
+      })
+    })
+
+    return resultsCounter
+  }
+
+  addMatchingRowsAndSamples = (inputStringRegExp, resultsByCategoryName, resultsCounter) => {
+    const matchingRowsByCategoryName = {} //used later to add samples from this row
+    this.props.rowsInCategories.forEach((category) => {
+      if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
+      const { categoryName } = category
+      category.rows.forEach((row) => {
+        if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
+        //add rows with matching names
+        if (inputStringRegExp.test(row.name)) {
+          matchingRowsByCategoryName[categoryName] = matchingRowsByCategoryName[categoryName] || []
+          matchingRowsByCategoryName[categoryName].push(row)
+
+          resultsByCategoryName[categoryName] = resultsByCategoryName[categoryName] || []
+          const newResult = {
+            action: this.isRowSelected(categoryName, row.name) ? 'REMOVE' : 'ADD',
+            categoryName,
+            rowName: row.name,
+          }
+          if (!_.some(resultsByCategoryName[categoryName], newResult)) { //avoid duplicates
+            resultsByCategoryName[categoryName].push(newResult)
+            resultsCounter += 1
+          }
+        }
+
+        resultsCounter = this.addMatchingSamplesFromRow(categoryName, row, inputStringRegExp, resultsByCategoryName, resultsCounter)
+
+      })
+    })
+
+    Object.entries(matchingRowsByCategoryName).forEach(([categoryName, rows]) => {
+      if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
+      resultsByCategoryName[categoryName] = resultsByCategoryName[categoryName] || []
+      rows.forEach((row) => {
+        if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
+        resultsCounter = this.addMatchingSamplesFromRow(categoryName, row, /.*/, resultsByCategoryName, resultsCounter)
+      })
+    })
+
+    return resultsCounter
+  }
+
   handleSearchChange = (e, { value }) => {
     this.setState({ isLoading: true, value })
-
-    const getSelectedRowNames = (categoryName) => (this.props.selectedRowNamesByCategoryName[categoryName] || [])
-    const isRowSelected = (categoryName, rowName) => getSelectedRowNames(categoryName).includes(rowName)
-
-    const getSelectedSamplesByRowName = (categoryName) => ((this.props.selectedSamplesByCategoryNameAndRowName[categoryName] || {}).selectedSamples || {})
-    const getSelectedSamplesForRow = (categoryName, rowName) => (getSelectedSamplesByRowName(categoryName)[rowName] || [])
-    const isSampleSelected = (categoryName, rowName, sample) => getSelectedSamplesForRow(categoryName, rowName).includes(sample)
 
     setTimeout(() => {
       if (this.state.value.length < 1) {
@@ -95,85 +208,16 @@ class SearchRows extends React.Component {
 
       // search actions
       if (inputStringRegExp.test('Hide')) {
-        Object.keys(this.props.selectedRowNamesByCategoryName).forEach((categoryName) => {
-          if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
-          this.props.selectedRowNamesByCategoryName[categoryName].forEach((rowName) => {
-            if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
-            resultsByCategoryName[categoryName] = resultsByCategoryName[categoryName] || []
-            const newResult = { action: 'REMOVE', categoryName, rowName }
-            if (!_.some(resultsByCategoryName[categoryName], newResult)) {
-              resultsByCategoryName[categoryName].push(newResult)
-            }
-            resultsCounter += 1
-          })
-        })
+        resultsCounter = this.addRowsToHide(resultsByCategoryName, resultsCounter)
       }
 
       if (inputStringRegExp.test('Hide') || inputStringRegExp.test('Hide sample')) {
-        Object.keys(this.props.selectedSamplesByCategoryNameAndRowName).forEach((categoryName) => {
-          if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
-          Object.keys(getSelectedSamplesByRowName(categoryName)).forEach((rowName) => {
-            if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS || !isRowSelected(categoryName, rowName)) return
-            getSelectedSamplesForRow(categoryName, rowName).forEach((sample) => {
-              if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
-              resultsByCategoryName[categoryName] = resultsByCategoryName[categoryName] || []
-              const newResult = { action: 'REMOVE', categoryName, rowName, sample }
-              if (!_.some(resultsByCategoryName[categoryName], newResult)) {
-                resultsByCategoryName[categoryName].push(newResult)
-              }
-              resultsCounter += 1
-            })
-          })
-        })
+        // hide samples
+        resultsCounter = this.addSamplesToHide(resultsByCategoryName, resultsCounter)
       }
 
       // search rows
-      this.props.rowsInCategories.forEach((category) => {
-        if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
-        category.rows.forEach((row) => {
-          if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
-          if (inputStringRegExp.test(row.name)) {
-            resultsByCategoryName[category.categoryName] = resultsByCategoryName[category.categoryName] || []
-            const newResult = {
-              action: isRowSelected(category.categoryName, row.name) ? 'REMOVE' : 'ADD',
-              categoryName: category.categoryName,
-              rowName: row.name,
-            }
-            if (!_.some(resultsByCategoryName[category.categoryName], newResult)) {
-              resultsByCategoryName[category.categoryName].push(newResult)
-            }
-            resultsCounter += 1
-          }
-
-          if (!row.data) {
-            return
-          }
-
-          const dataItemsWithSamples = row.data.filter((data) => data.samples && data.samples.length > 0)
-          if (dataItemsWithSamples.length > 1) {
-            console.warn('Found row with multiple data items each of which has a samples array. This may behave unexpectedly if there are duplicate samples')
-          }
-
-          dataItemsWithSamples.forEach((data) => {
-            (data.samples || []).forEach((sample) => {
-              if (resultsCounter >= MAX_AUTOCOMPLETE_RESULTS) return
-              if (inputStringRegExp.test(sample)) {
-                resultsByCategoryName[category.categoryName] = resultsByCategoryName[category.categoryName] || []
-                const newResult = {
-                  action: isRowSelected(category.categoryName, row.name) && isSampleSelected(category.categoryName, row.name, sample) ? 'REMOVE' : 'ADD',
-                  categoryName: category.categoryName,
-                  rowName: row.name,
-                  sample,
-                }
-                if (!_.some(resultsByCategoryName[category.categoryName], newResult)) {
-                  resultsByCategoryName[category.categoryName].push(newResult)
-                }
-                resultsCounter += 1
-              }
-            })
-          })
-        })
-      })
+      this.addMatchingRowsAndSamples(inputStringRegExp, resultsByCategoryName, resultsCounter)
 
       const useCategories = this.props.rowsInCategories.length > 1
       const results = useCategories ? {} : []
