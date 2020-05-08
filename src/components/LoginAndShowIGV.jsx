929f3032760418e7264c23e5225acdf60392fe2f
bw2 <weisburd@broadinstitute.org>
2020-05-08 18:26:24 -0400

    added info on storage buckets

--- src/components/LoginAndShowIGV.jsx
+++ src/components/LoginAndShowIGV.jsx
@@ -1,19 +1,31 @@
 /* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/no-unused-prop-types */
 /* eslint-disable react/require-optimization */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable operator-linebreak */
 
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import { GoogleLogin } from 'react-google-login'
-import { getGoogleUserEmail, googleSignIn, initGoogleClient } from '../utils/googleAuth'
 import IGV from './IGV'
+import { getGoogleUserEmail, googleSignIn, initGoogleClient } from '../utils/googleAuth'
+import { getInitialSettingsUrl, getRowsInCategories } from '../redux/selectors'
 
 const SignInButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 50px;
+`
+
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 50px;
+  margin-left: 100px;
+  word-wrap: break-word;
+  word-break: break-all;
 `
 
 class LoginAndShowIGV extends React.Component {
@@ -55,19 +67,62 @@ class LoginAndShowIGV extends React.Component {
     }
 
     if (this.state.show === 'signin-button') {
-      //https://developers.google.com/identity/sign-in/web/sign-in
-      //https://developers.google.com/identity/sign-in/web/build-button
+      let dataPathCounter = 0
+      const buckets = new Set()
+      this.props.rowsInCategories.forEach((category) => {
+        category.rows.forEach((row) => {
+          if (row.data) {
+            row.data.forEach((data) => {
+              if (data.url && data.url.startsWith('gs://')) {
+                dataPathCounter += 1
+
+                const urlTokens = data.url.split('/')
+                const bucket = urlTokens[2]
+                if (bucket !== 'tgg-viewer') {
+                  buckets.add(bucket)
+                }
+              }
+            })
+          }
+        })
+      })
+
+      if (dataPathCounter === 0) {
+        return <InfoContainer>No dataset paths loaded. &nbsp; Please specify an Initial settings url.</InfoContainer>
+      }
+
       return (
-        <SignInButtonContainer>
-          <GoogleLogin
-            clientId={window.TGG_VIEWER_CLIENT_ID}
-            theme="dark"
-            buttonText="Sign In to Google"
-            onSuccess={this.handleGoogleSignIn}
-            onFailure={this.handleGoogleSignIn}
-            cookiePolicy="single_host_origin"
-          />
-        </SignInButtonContainer>)
+        <div>
+          <InfoContainer>
+            <div>
+              Retrieved {dataPathCounter} data paths from settings file: <a href={this.props.initialSettingsUrl} target="_blank">{this.props.initialSettingsUrl}</a><br />
+              <br />
+            </div>
+
+            {
+              buckets.size > 0 &&
+              <div>
+                To view this data, you should have read access to the following Google Storage buckets:
+                <ul>
+                  {
+                    Array.from(buckets).map((bucket) => <li key={bucket}><a href={`https://console.cloud.google.com/storage/browser/${bucket}`} target="_blank">{bucket}</a></li>)
+                  }
+                </ul>
+              </div>
+            }
+          </InfoContainer>
+          <SignInButtonContainer>
+            <GoogleLogin
+              clientId={window.TGG_VIEWER_CLIENT_ID}
+              theme="dark"
+              buttonText="Sign in with Google"
+              onSuccess={this.handleGoogleSignIn}
+              onFailure={this.handleGoogleSignIn}
+              cookiePolicy="single_host_origin"
+            />
+          </SignInButtonContainer>
+        </div>)
+
     }
 
     return null
@@ -75,9 +130,15 @@ class LoginAndShowIGV extends React.Component {
 }
 
 LoginAndShowIGV.propTypes = {
+  rowsInCategories: PropTypes.array,
+  initialSettingsUrl: PropTypes.string,
   userChangedHandler: PropTypes.func,
 }
 
+const mapStateToProps = (state) => ({
+  rowsInCategories: getRowsInCategories(state),
+  initialSettingsUrl: getInitialSettingsUrl(state),
+})
 
 const mapDispatchToProps = (dispatch) => ({
   userChangedHandler: (googleUserEmail) => {
@@ -89,4 +150,4 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 
-export default connect(null, mapDispatchToProps)(LoginAndShowIGV)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginAndShowIGV)
