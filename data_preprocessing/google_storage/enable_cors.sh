27aaebbaeae87469e04438a06b265eac71c107f5
bw2 <bw2@users.noreply.github.com>
2021-06-30 15:45:16 -0400

    Update enable_cors.sh

--- data_preprocessing/google_storage/enable_cors.sh
+++ data_preprocessing/google_storage/enable_cors.sh
@@ -1,15 +1,21 @@
-# If you'd like to put TGG-viewer json configs in a google storage bucket and make them publicly-accessible so that they can be passed in to the "Initial settings" field,
-# you can do this by:
-# 1. uploading the .json config(s) to your bucket
-# 2. setting bucket contents to be public by granting read access to "AllUsers", or,
+# TGG-viewer allows it's sample lists, settings, and selected tracks to be saved or loaded to/from a json config file. 
+# To load or restore settings from such as config file, you have to store it at a publicly-accessible url, and then paste this url into the "Initial settings"
+# field in TGG-viewer. 
+# One option is to store the config in a public github repository. Another is to put it in a google storage bucket. 
+# For the second option, the steps below adjust the google bucket's settings so that it works with TGG-viewer.
+# 
+# 1. upload the .json config(s) to gs://your-bucket
+# 2. set bucket contents to be public by granting read access to "AllUsers", or,
 #    if you chose to enable ACLs when creating your bucket, you can make just the config(s) public by running:
 #
 #     gsutil acl ch -u AllUsers:R gs://your-bucket/your-config.json
 #
-# 3. updating CORS settings for the bucket by running this script:
+# 3. update the bucket's CORS settings by running this script:
 #
-#     ./enable_cors.sh gs://you-bucket
-
+#     ./enable_cors.sh gs://your-bucket
+# 
+# 4. paste the config url (eg. https://storage.googleapis.com/your-bucket/path/your-config.json
+# 
 BUCKET=$1   # eg. "gs://my-bucket"
 gsutil cors set <( echo '[{ "origin": ["*"], "method": ["GET"] }]' ) $BUCKET
 gsutil cors get $BUCKET
